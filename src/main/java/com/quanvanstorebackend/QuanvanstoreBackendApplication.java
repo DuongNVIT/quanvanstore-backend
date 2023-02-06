f652857828f55c4b37898a956fb5ae0e7dd0ac40
Duong <nguyenduong985911@gmail.com>
2023-02-06 23:00:38 +0700

    initial commit

---
+++ src/main/java/com/quanvanstorebackend/QuanvanstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quanvanstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanvanstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanvanstoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quanvanstorebackend/QuanvanstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanvanstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanvanstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
